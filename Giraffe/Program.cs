dec84061508903e2664f615278eb944d970a7355
russtack <stirlingest@gmail.com>
2023-04-07 08:38:56 -0400

    csharp

---
+++ Giraffe/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        string characterName = "John Wick";
+        int age = 56;
+        char grade = 'A';
+        bool isKiller = true;
+        Console.WriteLine("Hello "+ characterName);
+        Console.WriteLine("My age is " + age);
+        Console.WriteLine("Efffin " + grade);
+        Console.WriteLine("Efffin " + isKiller);
+    }
+}
