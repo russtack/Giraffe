7ae212facd31e92d98b282589d54104e385a9482
russtack <stirlingest@gmail.com>
2023-04-07 22:43:35 -0400

    calculator

--- Giraffe/Program.cs
+++ Giraffe/Program.cs
@@ -2,13 +2,25 @@
 {
     public static void Main(string[] args)
     {
-        string characterName = "John Wick";
-        int age = 56;
-        char grade = 'A';
-        bool isKiller = true;
-        Console.WriteLine("Hello "+ characterName);
-        Console.WriteLine("My age is " + age);
-        Console.WriteLine("Efffin " + grade);
-        Console.WriteLine("Efffin " + isKiller);
+        Console.WriteLine("enter a number: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("enr operator: ");
+        string op = Console.ReadLine();
+        Console.WriteLine("enr sec numbah: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        if (op == "-")
+        {
+            Console.WriteLine(num1 - num2);
+        }
+        else if (op == "+") { Console.WriteLine(num1 + num2); }
+        else if(op == "*") { Console.WriteLine(num1 * num2); }
+        else if (op == "/") { Console.WriteLine(num1 / num2); }
+        else { Console.WriteLine("invalid operator"); }
+
+        
+        
     }
+   
 }
